490502477187a5778644e8303f92ba01c9524903
Sean <butts.sean@gmail.com>
2015-09-29 08:13:21 -0400

    debug code

--- com/learnsecurity/SimpleWebServer.java
+++ com/learnsecurity/SimpleWebServer.java
@@ -201,7 +201,7 @@ public class SimpleWebServer {
         Map<String, String> headerMap = new HashMap<String, String>();
 
         String line;
-        while (!(line = headerReader.readLine()).equals("")){
+        while (!(line = headerReader.readLine()).isEmpty()){
             System.out.println(line+"*");
             String[] headerValueSplit = line.split(": ");
             String name = headerValueSplit[0];
